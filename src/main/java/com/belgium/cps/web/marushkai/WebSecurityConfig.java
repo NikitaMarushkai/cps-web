0b1285615e86a4ba697b31e2823e3f944810d747
NikitaMarushkai <unlimited54321@mail.ru>
2017-06-29 00:33:49 +0500

    added security

---
+++ src/main/java/com/belgium/cps/web/marushkai/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.belgium.cps.web.marushkai;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by unlim_000 on 26.06.2017.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("cps_admin").password("cps_523-ADMIN_^3__2134dsf$").roles("ADMIN");
+    }
+}
